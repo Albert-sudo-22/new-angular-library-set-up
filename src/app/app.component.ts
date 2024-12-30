11b3d6df6d0ab78cb5ef8d7040bc51751166597a
Albert <krm9346@autuni.ac.nz>
2024-12-30 11:44:25 +0700

    update autocomplete, badge and bottom sheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BadgeOverviewExample } from './Badge/badge-overview-example.component';
+import { AutocompleteRequireSelectionExample } from './AutoComplete/Required autoComplete/autocomplete-require-selection-example.component';
+import { AutocompleteSimpleExample } from './AutoComplete/Simple/autocomplete-simple-example.component';
+import { BottomSheetOverviewExample } from './Bottom Sheet/bottom-sheet-overview-example.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BadgeOverviewExample, AutocompleteRequireSelectionExample
+    ,AutocompleteSimpleExample,BottomSheetOverviewExample
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
