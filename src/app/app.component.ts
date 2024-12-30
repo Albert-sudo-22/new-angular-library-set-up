c8296daaf066f195a6ebcdfaab3d8c402664c61f
Albert <krm9346@autuni.ac.nz>
2024-12-30 11:51:13 +0700

    update buttons and button toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { BadgeOverviewExample } from './Badge/badge-overview-example.component';
 import { AutocompleteRequireSelectionExample } from './AutoComplete/Required autoComplete/autocomplete-require-selection-example.component';
 import { AutocompleteSimpleExample } from './AutoComplete/Simple/autocomplete-simple-example.component';
 import { BottomSheetOverviewExample } from './Bottom Sheet/bottom-sheet-overview-example.component';
+import { ButtonOverviewExample } from './Buttons/button-overview-example.component';
+import { ButtonToggleModeExample } from './Buttons toggle/button-toggle-mode-example.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, BadgeOverviewExample, AutocompleteRequireSelectionExample
-    ,AutocompleteSimpleExample,BottomSheetOverviewExample
+    ,AutocompleteSimpleExample,BottomSheetOverviewExample, ButtonOverviewExample,
+    ButtonToggleModeExample
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
